669c9355038704becb55dd3b2c5372bbe58dd9c1
giduthuris <giduthuri.srini@gmail.com>
2025-02-09 05:13:57 +0530

    HIS-134 fix added

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -9,6 +9,7 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		ConfigurableApplicationContext run = SpringApplication.run(DemoApplication.class, args);
+		// changes for HIS-134
 		run.close();
 	}
 
